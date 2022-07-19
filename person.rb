892001ff55c23f20a1ee1160360841fad3ce7663
V0pe <dampblew01@gmail.com>
2022-07-19 08:53:30 -0400

    class composited

--- person.rb
+++ person.rb
@@ -1,9 +1,16 @@
-class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+class Nameable
+  def correct_name
+    @name
+  end
+end
+
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission=true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   attr_accessor :name, :age
