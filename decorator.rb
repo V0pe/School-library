e7ba0de1c72f535c497cf914a8e33ea1621198d6
V0pe <dampblew01@gmail.com>
2022-07-19 12:44:20 -0400

    add the inheritance

--- decorator.rb
+++ decorator.rb
@@ -1,27 +1,28 @@
 require './person'
 
-  class Decorator < Nameable
-    def initialize(nameable)
-      @nameable = nameable
-    end
-  
-    def correct_name
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.upcase!
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name > 10
+      @nameable.correct_name[0...10]
+    else
       @nameable.correct_name
     end
   end
-  
-  class CapitalizeDecorator < Decorator
-    def correct_name
-      @nameable.correct_name.upcase!
-    end
-  end
-
-  class TrimmerDecorator < Decorator
-    def correct_name
-      if @nameable.correct_name > 10
-        @nameable.correct_name[0...10]
-      else
-        @nameable.correct_name
-      end 
-    end
-  end
+end
