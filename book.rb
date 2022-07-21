08cb7410648e9d82773dbd3e49595f5b5666ab81
V0pe <dampblew01@gmail.com>
2022-07-21 06:21:00 -0400

    linters fixed

--- book.rb
+++ book.rb
@@ -1,17 +1,17 @@
 require './rentals'
 
-class Book
-    attr_reader :rentals
-    attr_accessor :title, :author
-  
-    def initialize(title, author)
-      @title = title
-      @author = author
-      @rentals = []
-    end
-    
-    #add the rentals and a book has many rentals
-    def add_rental(date, person)
-        Rental.new(date, person,self)
-      end
+class Bo
+  attr_reader :rentals
+  attr_accessor :title, :author
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
   end
+
+  # add the rentals and a book has many rentals
+  def add_rental(date, person)
+    Rental.new(date, person, self)
+  end
+end
--- rentals.rb
+++ rentals.rb
@@ -2,17 +2,17 @@ require './person'
 require './book'
 
 class Rentals
-    attr_reader :person, :book
-    attr_accessor :date,
-  
-    def initialize(date, person, book)
-      @date = date
-      
-      #many to many relationships for person and books
-      @person = person
-      person.rentals << self
-  
-      @book = book
-      book.rentals << self
-    end
+  attr_reader :person, :book
+  attr_accessor :date
+
+  def initialize(date, person, book)
+    @date = date
+
+    # many to many relationships for person and books
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
   end
+end
